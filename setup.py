c45efcc9227b6fdfbf166d0482ddca647e098611
Matt Dennewitz <mattdennewitz@gmail.com>
2014-05-07 16:16:22 -0500

    1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'retrosheet-pitch-sequences',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Retrosheet at-bat ball/strike count state extraction',
     author = 'Matt Dennewitz',
     author_email = 'mattdennewitz@gmail.com',
